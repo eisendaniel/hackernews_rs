c70f9df017c5b60f4677d5620ed2627e3efe1d8f
Daniel Eisen <38846425+eisendaniel@users.noreply.github.com>
2022-11-10 15:36:25 +1300

    time, pts, proper link simplif

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
-use crate::frame_history::FrameHistory;
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use eframe::egui;
 use poll_promise::Promise;
 use serde::{Deserialize, Serialize};
@@ -48,7 +49,7 @@ impl Card {
         let ctx = ctx.clone();
         let (sender, story_promise) = Promise::new();
         let request = ehttp::Request::get(format!(
-            "https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty",
+            "https://hacker-news.firebaseio.com/v0/item/{}.json",
             id
         ));
         ehttp::fetch(request, move |response| {
@@ -62,15 +63,42 @@ impl Card {
     }
 
     fn draw(&mut self, ui: &mut egui::Ui, ctx: &egui::Context) {
-        if let Some(result) = self.story_promise.ready() {
+        if let Some(result) = self.story_promise.ready_mut() {
             match result {
                 Ok(story) => {
-                    ui.heading(&story.title);
-                    let url = match story.url.clone() {
-                        Some(url) => url,
+                    let url = match &story.url {
+                        Some(url) => url.to_owned(),
                         None => format!("https://news.ycombinator.com/item?id={}", self.id),
                     };
-                    ui.hyperlink(&url);
+                    let url = url::Url::parse(&url).expect(&format!("{:?}", story.url));
+
+                    ui.heading(&story.title);
+                    ui.horizontal(|ui| {
+                        ui.label(format!("{} points  ⚫", story.score));
+                        ui.hyperlink_to(url.domain().unwrap_or(url.as_str()), url.as_str());
+                        let mins = (SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .expect("Time Travel")
+                            .as_secs()
+                            - story.time)
+                            / 60;
+                        let when = if mins < 1 {
+                            "⚫  just now".into()
+                        } else if mins < 2 {
+                            "⚫  1 min".into()
+                        } else if mins < 60 {
+                            format!("⚫  {} mins", mins)
+                        } else if mins < 120 {
+                            "⚫  1 hr".into()
+                        } else if mins < 1440 {
+                            format!("⚫  {} hrs", mins / 60)
+                        } else if mins < 2880 {
+                            "⚫  1 day".into()
+                        } else {
+                            format!("⚫  {} days", mins / 1440)
+                        };
+                        ui.label(when);
+                    });
                 }
                 Err(_) => {
                     self.story_promise = Self::new(self.id, ctx).story_promise;
@@ -94,7 +122,6 @@ impl Card {
 }
 
 pub struct MainApp {
-    _frame_history: FrameHistory,
     stories_promise: Option<Promise<ehttp::Result<Vec<Card>>>>,
     category: Catagory,
 }
@@ -102,7 +129,6 @@ pub struct MainApp {
 impl Default for MainApp {
     fn default() -> Self {
         Self {
-            _frame_history: FrameHistory::default(),
             stories_promise: None,
             category: Catagory::Top,
         }
@@ -150,24 +176,11 @@ impl eframe::App for MainApp {
 }
 
 impl MainApp {
-    fn _display_fps(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
-        self._frame_history
-            .on_new_frame(ctx.input().time, frame.info().cpu_usage);
-
-        //variable frame rate: min of 1fps and a max of vsync_max or event_count/s
-        ctx.request_repaint_after(std::time::Duration::from_millis(900));
-        frame.set_window_title(&format!(
-            "{:.2} fps, {:.2} ms / frame",
-            self._frame_history.fps(),
-            self._frame_history.mean_frame_time() * 1000.
-        ));
-    }
-
     fn refresh_stories(&mut self, ctx: &egui::Context) {
         let ctx = ctx.clone();
         let (sender, promise) = Promise::new();
         let request = ehttp::Request::get(format!(
-            "https://hacker-news.firebaseio.com/v0/{}stories.json?print=pretty",
+            "https://hacker-news.firebaseio.com/v0/{}stories.json",
             &self.category.to_string().to_lowercase()
         ));
         ehttp::fetch(request, move |response| {
